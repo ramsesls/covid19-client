9285bf7fcaa4e1cb91599bc0cc2da745259e12ee
Suren Atoyan <contact@surenatoyan.com>
2020-03-21 20:59:19 +0400

    check ie and show message

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import 'react-app-polyfill/ie9';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import 'proxy-polyfill';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+Promise.all([
+  import('react'),
+  import('react-dom'),
+  import('./App'),
+]).then(([
+  { default: React },
+  { default: ReactDOM },
+  { default: App },
+]) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
